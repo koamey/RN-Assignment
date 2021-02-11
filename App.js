9d0ed521ad296141c8b6c06309960a98562c85b7
Amey K <ameyk@dynamischit.com>
2021-02-11 11:39:32 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
+import 'react-native-gesture-handler';
 import React, {Component} from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 import Navigator from './routes/RouterModule';
 
 export default class App extends Component {
